15d24f2f6780b6e5752e5fc698afe76df4439b4e
tomas <twm1@alu.ua.es>
2024-10-18 14:38:08 +0200

    poca cosa

--- script/formCheck.js
+++ script/formCheck.js
@@ -1,4 +1,28 @@
-
 document.addEventListener("DOMContentLoaded", function(){
-    
+	let form = document.querySelector("#formLogin");
+	form.addEventListener("submit", comprobarLogin);
 });
+
+function comprobarLogin(event){
+	let mensaje = "";
+	let name = document.querySelector("#userName").value;
+	let password = document.querySelector("#pass").value;
+	let form = document.querySelector("#formLogin");
+	name = name.replaceAll(/\s/g, "");
+	name = name.replaceAll(/\t/g, "");
+	password = password.replaceAll(/\s/g, "");
+	password = password.replaceAll(/\t/g, "");
+	
+	if (name == "" ) {
+		mensaje += "El campo nombre no puede estar vacio\n";
+	}
+	if (password == "") {
+		mensaje += "El campo contraseña no puede estar vacio\n";
+	}
+	if (mensaje != "") {
+		let p = document.createElement("p");
+		p.innerHTML = mensaje.replace(/\n/g, "<br>");
+		form.prepend(p);
+		event.preventDefault();
+	}
+}
