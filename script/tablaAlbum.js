b479c2316bd8ec0f30775628f4d5a2975104dfb1
alexvalmu <valdelvira6@gmail.com>
2024-10-22 12:45:35 +0200

    empiezo tabla

---
+++ script/tablaAlbum.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttonShow = document.getElementById("show");
+    buttonShow.addEventListener("click", function() {
+        console.log("se hizo click en el boton de mostrar tabla");
+    });
+    
+});
