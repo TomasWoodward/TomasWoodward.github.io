8a6c11dd448f85959278fa773508db0709445684
alexvalmu <valdelvira6@gmail.com>
2024-11-16 17:46:10 +0100

    sino remember logout no funciona

--- logOut.php
+++ logOut.php
@@ -6,6 +6,7 @@
         setcookie("password", "", time() - 3600);
         setcookie("lastVisit", "", time() - 3600);
         session_unset();
+        session_destroy();
     }
 
     header("Location: index.php");
