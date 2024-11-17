a216988bbf71401bd22dbf1fc72ff2f5e7cb594d
tomas <twm1@alu.ua.es>
2024-11-17 13:35:47 +0100

    CAMBIO A MVC

--- inc/footer.php
+++
@@ -1,4 +0,0 @@
-<footer  data-url="htpps://dawua.free.nf/">
-        <p><a href="https://creativecommons.org/licenses/">&copy;</a> Develop by Tomas Woodward Marin y Alex Valdelvira Muñoz 2024. All rights reserved </p>
-        <p><a href="accesibility.php">Accesibility statements</a></p>
-    </footer>
--- inc/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-        <a href="index.php"><img src="img/system/Logo.png" alt="">
-        <h1>PI-Pictures &amp; Images</h1></a>
-</header>
